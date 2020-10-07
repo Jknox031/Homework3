88369f72e5c14c8ce113fd5a8f8a07db1d8ab040
James Knox <jknox031@me.com>
2020-10-07 15:51:22 -0400

    fixing onclick button

--- script.js
+++ script.js
@@ -1,41 +1,18 @@
-// Assignment Code
-let generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-Form.addEventListener("generate", e => {
-  e.preventDefault()
-})
-
-
-
-
-
-
-function generate(){
-
+function generate() {
   // set password length/complexity
   let complexity = document.getElementById("CharacterAmountNumber").value;
   //possible password values
-  let values = abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_-=+\;
+  let values = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-=";
 
   let password = "";
 
   //creat for loop for password characters
-  for(var i = 0; i <= complexity; i++){
-    password = password + values.charAt(math.random() * math.floor(value.length -1));
+  for (var i = 0; i <= complexity; i++) {
+    password =
+      password +
+      values.charAt(math.floor(math.random() * math.floor(values.length - 1)));
   }
 
-
   //add password to display area
-  document.getElementById("generated password").value = password;
-
+  document.getElementById("Generated Password").value = password;
 }
